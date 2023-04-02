6c38a6357c00b61e17168033f92470cb72ced71a
AYCXJY <97771214+AYCXJY@users.noreply.github.com>
2023-04-02 13:42:48 +0800

    Update main.py
    Add some space between code lines

--- 002_模板匹配识别数字/main.py
+++ 002_模板匹配识别数字/main.py
@@ -10,12 +10,16 @@ from image import SEARCH_EX, SEARCH_DS
 
 # 初始化感光元件
 sensor.reset()
+
 # 设置图像对比度为1
 sensor.set_contrast(1)
+
 # 图像增益上限设置为16
 sensor.set_gainceiling(16)
+
 # 模板匹配最大分辨率为QQVGA
 sensor.set_framesize(sensor.QQVGA)
+
 # 像素格式为灰度
 sensor.set_pixformat(sensor.GRAYSCALE)
 
@@ -25,6 +29,7 @@ templates = ("/1.pgm", "/2.pgm", "/3.pgm", "/4.pgm")
 while (True):
     # 拍摄一张照片，得到图像对象
     img = sensor.snapshot()
+    
     # 轮询搜索目标模板，匹配成功画矩形
     for index in templates:
         template = image.Image(index)
